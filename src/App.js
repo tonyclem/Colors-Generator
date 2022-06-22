56ca13fcdc9230c79fbd923b2fe50acd2b1b299f
tonyclem <tonyclem001@gmail.com>
2022-06-22 18:58:58 +0200

    added copy to dashboard

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import Values from "values.js";
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#222").all(5));
 
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
-      let colors = new Values(color).all(10);
+      let colors = new Values(color).all(5);
       setList(colors);
       setError(false);
     } catch (error) {
@@ -29,7 +29,7 @@ function App() {
             type="text"
             value={color}
             onChange={(e) => setColor(e.target.value)}
-            placeholder="#f15025"
+            placeholder="#222"
             className={`${error ? "error" : null}`}
           />
           <button className="btn" type="submit">
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -6,13 +6,28 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const bcg = rgb.join(",");
   const hexValue = `#${hexColor}`;
   // const hex = rgbToHex(...rgb);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [alert]);
+
   return (
     <article
       className={`color  ${index > 10 && "color-light"}`}
       style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
     </article>
   );
 };
